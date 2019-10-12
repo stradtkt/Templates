2559ec0225f7911dda65f1420d3259574e3a6507
Kevin <stradtkt22@gmail.com>
2019-10-12 03:31:19 -0400

    Added starter code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+using namespace std;
+
+template<class T, class K>
+class Test {
+private:
+    T obj;
+public:
+    Test(T obj) {
+        this->obj = obj;
+    }
+    void print() {
+        cout << obj << endl;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Test<string, int> test1("Hello");
+    test1.print();
     return 0;
 }
